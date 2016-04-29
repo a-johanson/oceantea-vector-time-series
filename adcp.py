2b93dcdb66cb42b7fed14c55a0e3bd62b494d118
arj <arj@informatik.uni-kiel.de>
2016-04-29 14:20:37 +0200

    numpy slicing

---
+++ adcp.py
@@ -0,0 +1,92 @@
+import numpy as np
+from io import BytesIO
+from threading import Lock
+import pickle as pickle
+import json as json
+import math as math
+
+adcpLock = Lock()
+adcpDBPath = "data/adcp_db.pickle"
+adcpDB = {}
+
+def adcpReadDB():
+	global adcpDB
+	try:
+		adcpDB = pickle.load(open(adcpDBPath, "rb"))
+	except:
+		adcpDB = {}
+
+adcpReadDB()
+
+def adcpWriteDB(acquireLock=True):
+	global adcpDB
+	
+	if acquireLock:
+		while not adcpLock.acquire():
+			pass
+	
+	try:
+		pickle.dump(adcpDB, open(adcpDBPath, "wb"))
+	except:
+		pass
+	
+	if acquireLock:
+		adcpLock.release()
+
+def adcpUpdateDB(station, nBins, startDepth, firstBinHeight, binHeight):
+	global adcpDB
+	if not (station in adcpDB):
+		adcpDB[station] = {}
+	adcpDB[station]["nBins"] = nBins
+	adcpDB[station]["startDepth"] = startDepth
+	adcpDB[station]["firstBinHeight"] = firstBinHeight
+	adcpDB[station]["binHeight"] = binHeight
+	adcpWriteDB()
+
+
+def adcpGetFileName(station):
+	return "data/adcp-dirmag-{}.npy".format(station)
+	
+def adcpStore(station, dataSet):
+	#TODO: Error handling, return True/False
+	dataSet.tofile(adcpGetFileName(station))
+	return True
+
+# import dirmag
+def adcpImport(station, startDepth, firstBinHeight, binHeight, csvBytes):
+	#TODO: Error handling
+	dataSet = np.genfromtxt(BytesIO(csvBytes), delimiter=",", skip_header=1)
+	nCols = dataSet.shape[1]
+	print(nCols)
+	if nCols < 3 or nCols%2 != 1:
+		return False
+	nBins = (nCols-1)/2
+	if adcpStore(station, dataSet):
+		adcpUpdateDB(station, nBins, startDepth, firstBinHeight, binHeight)
+		return True
+	return False
+	
+
+	
+def adcpLoad(station, nCols):
+	#TODO: Error handling
+	fileName = adcpGetFileName(station)
+	return np.fromfile(fileName).reshape((-1, nCols))
+	
+
+def adcpGetJSONSeries(station, depth):
+	global adcpDB
+	if not (station in adcpDB):
+		return json.dumps({"data":[]})
+	
+	distance = adcpDB[station]["startDepth"] - depth
+	iBin = math.ceil(max(distance - adcpDB[station]["firstBinHeight"], 0.0) / adcpDB[station]["binHeight"])
+	if iBin >= adcpDB[station]["nBins"] or distance < 0.0:
+		return json.dumps({"data":[]})
+	
+	dataSet = adcpLoad(station, 1 + 2*adcpDB[station]["nBins"])
+	result = np.empty((dataSet.shape[0], 4))
+	result[:,[0,2,3]] = dataSet[:, [0, 1+iBin, 1+adcpDB[station]["nBins"]+iBin]]
+	result[:,1] = depth 
+	return json.dumps({"data": result.tolist()})
+
--- server.py
+++ server.py
@@ -2,20 +2,36 @@ from flask import Flask
 from flask import request
 import scipy as sp
 from io import BytesIO
+from threading import Lock
+import adcp as adcp
 
 app = Flask(__name__)
 app.debug = True
+
+csvString = """a,b,c,d,e
+1,2,3,4,5
+6,7,8,9,10
+11,12,13,14,15"""
+adcp.adcpImport("test", 100, 6.16, 4.0, csvString.encode())
+
+print(adcp.adcpGetJSONSeries("test", 99.5))
 
 def getADCPDataFileName(station):
     return "adcp_data/adcp-dirmag-{}.rds".format(station)
 
 @app.route('/<station>', methods=["POST"])
 def upload_station(station):
+    #TODO: check and sanitize inputs
     #decoded = request.get_data().decode("utf-8")
-    inputData = sp.genfromtxt(BytesIO(request.get_data()), delimiter=",")
-    return sp.array2string(inputData)
+    startDepth = request.args.get("startDepth")
+    firstBinHeight = request.args.get("firstBinHeight")
+    binHeight = request.args.get("binHeight")
+    adcp.adcpImport(station, startDepth, firstBinHeight, binHeight, request.get_data())
+    return
+    
+#TODO: add enpoints
 
-if __name__ == '__main__':
-    #app.run(host='0.0.0.0')
-    app.run(port=3334)
+#if __name__ == '__main__':
+#    #app.run(host='0.0.0.0')
+#    app.run(threaded=True, port=3334)
  
